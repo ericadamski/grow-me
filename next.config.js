6ab36ed01a2f110493cece222639256170f20b1a
Eric Adamski <er.adamski@gmail.com>
2019-05-15 13:36:35 -0400

    Update the base url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const withCSS = require("@zeit/next-css");
 module.exports = withCSS({
   env: {
     BASE_URL:
-      process.env.NODE_ENV === "development" ? "http://localhost:3000" : "",
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : "https://grow-me.now.sh",
   },
   target: "serverless",
 });
